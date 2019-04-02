b498cfc0271615c15b717c20a57553dfd9aa0a2f
Alexey Davydov <falcemmortis@gmail.com>
2019-04-02 15:55:04 +0300

    Add documneting comments
    Add comments for adding, substraction, multiplication
    
    Signed-off-by: Alexey Davydov <falcemmmortis@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,19 @@
 #include "calculator.h"
+//implementaion of calculator
 
+// Add to doubles
 int Calculator::Add (double a, double b)
 {
     return a + b + 0.5;
 }
 
+// Substract one from another
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+// Multiplicate one double with another
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
